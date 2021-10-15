3a4c7cc76f49b02f46980b9a5bd2096b5e06a158
DarkDhz <blazegammer24@gmail.com>
2021-10-15 12:41:35 +0200

    SP1-06
    Finished Get ProductList:
    - added methods for encryption

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_restful import Api
 from resources.Product import ProductResource, UserProductResource, UserProductListResource
+from resources.User import UserAccount
+from flask import session
+from utils.security import secret_key
 
-# import mysql.connector
+
+
+# https://flask.palletsprojects.com/en/2.0.x/quickstart/#sessions
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
 api = Api(app)
-
-
 
 
 @app.route('/')
@@ -15,11 +19,10 @@ def mainPage():
     return 'Main page of ubending!'
 
 
+api.add_resource(UserAccount, '/user/<int:user_id>')
 api.add_resource(ProductResource, '/product/<int:product_id>')
 api.add_resource(UserProductResource, '/user/<int:user_id>/product/<int:product_id>', "/user/<int:user_id>/product")
-api.add_resource(UserProductListResource, '/user/<int:user_id>/products')
+api.add_resource(UserProductListResource, '/user/<int:user_id>/products', methods=['GET'])
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
-
-
