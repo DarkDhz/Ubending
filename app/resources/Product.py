7020b5ee47e3c8f678268efeb9b70ee3c10de2a3
DarkDhz <blazegammer24@gmail.com>
2021-10-21 20:26:22 +0200

    SP1-08
    Added mysql remote server

--- app/resources/Product.py
+++ app/resources/Product.py
@@ -13,30 +13,10 @@ class ProductResource(Resource):
             return result, 200
 
     def post(self, product_id, user_id):
-        parser = reqparse.RequestParser()  # create parameters parser from request
-        parser.add_argument('product_id', type=str, required=True, help="This field cannot be left blank")
-        parser.add_argument('owner_id', type=str, required=True, help="This field cannot be left blank")
-        parser.add_argument('name', type=str, required=True, help="This field cannot be left blank")
-        parser.add_argument('description', type=str, required=True, help="This field cannot be left blank")
-        parser.add_argument('price', type=float, required=True, help="This field cannot be left blank")
-        parser.add_argument('state', type=int, required=True, help="This field cannot be left blank")
-        parser.add_argument('image', type=werkzeug.datastructures.FileStorage, location='files')
-        parser.add_argument('category_id', type=int)
-
-        data = parser.parse_args()
-        # We cannot have a negative price.
-        if data['price'] < 0:
-            return {'message': "Price attribute cannot be negative."}, 409
-        addProduct(data)
-        return {'message': "Product added successfully"}, 200
+        return {'message': "Not developed yet"}, 404
 
     def delete(self, user_id, product_id):
-        product = getProductByIds(user_id=user_id, product_id=product_id)
-        if product is not None:
-            deleteProduct(product_id)
-            return {'message': "Product with id [{}] deleted successfully".format(product_id)}, 200
-        else:
-            return {'message': "Product with id [{}] doest not exist".format(product_id)}, 404
+        return {'message': "Not developed yet"}, 404
 
     def put(self, id):
         return {'message': "Not developed yet"}, 404
@@ -97,7 +77,7 @@ class UserProductResource(Resource):
         parser.add_argument('description', type=str)
         parser.add_argument('price', type=int)
         parser.add_argument('state', type=int)
-        parser.add_argument('image', type=bin)
+        parser.add_argument('image', type=werkzeug.datastructures.FileStorage)
         parser.add_argument('category', type=int)
 
         data = parser.parse_args()
