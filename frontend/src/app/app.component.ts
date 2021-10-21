7f34c624597f17186cbcd23ee101b53e8291d408
Estibaliz26 <estibalizimc@gmail.com>
2021-10-21 20:09:25 +0200

    Grid of cards products responsive

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -28,9 +28,29 @@ export class AppComponent implements OnInit{
       })
   }
   url:string = "../assets/img2.jpg"
+  url1:string = "../assets/img2.jpg"
+  url2:string = "../assets/img2.jpg"
+  url3:string = "../assets/img2.jpg"
+  url4:string = "../assets/img2.jpg"
 
   changeImage(event:any){
     this.url = event.target.src;
+  }
+
+  changeImage1(event:any){
+    this.url1 = event.target.src;
+  }
+
+  changeImage2(event:any){
+    this.url2 = event.target.src;
+  }
+
+  changeImage3(event:any){
+    this.url3 = event.target.src;
+  }
+
+  changeImage4(event:any){
+    this.url4 = event.target.src;
   }
 
 }
