0d9e1a9638df20eabbb335aec3b98cc94ecd55e3
Estibaliz26 <estibalizimc@gmail.com>
2021-10-22 13:25:01 +0200

    Changed images path in order to render in heroku app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import {SearchBarComponent} from "./components/search-bar/search-bar.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import { addItemComponent } from "./components/addItem/addItem.component";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+/*import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';*/
 
 @NgModule({
     declarations: [
@@ -16,7 +16,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    FontAwesomeModule
+    /*FontAwesomeModule*/
   ],
   providers: [],
   bootstrap: [AppComponent]
