4ca3d898d326f4f333f12316c35b3fe9c51e6690
Adria de Juan <adriadejuan22@gmail.com>
2021-11-06 18:13:51 +0100

    falta enlazar formulario al boton de basura2.0

--- app/resources/User.py
+++ app/resources/User.py
@@ -1,12 +1,43 @@
 from flask_restful import Resource, reqparse
+from data.UserQueries import checkPasswords, getAccountByEmail, addUserToDB
 
 class UserAccount(Resource):
 
     def get(self, id):
         return {'message': "Not developed yet"}, 404
 
-    def post(self, id):
-        return {'message': "Not developed yet"}, 404
+    def post(self):
+
+        parser = reqparse.RequestParser()  # create parameters parser from request
+
+        parser.add_argument('email', type=str, required=True, help="This field cannot be left blank")
+        parser.add_argument('password', type=str, required=True, help="This field cannot be left blank")
+        parser.add_argument('repeat_password', type=float, required=True, help="This field cannot be left blank")
+
+        data = parser.parse_args()
+
+        result = getAccountByEmail(data['email'])
+
+        if result != 404:
+            return {'Email already  registered.'}
+
+        pwCode = checkPasswords(data['password'], data['repeat_password'])
+
+        if pwCode == 1:
+            return {"Passwords do not match."}, 400
+        elif pwCode == 2:
+            return {"Your password must be at least 8 characters long."}, 400
+        elif pwCode == 3:
+            return {"Your password must have at least 1 number"}, 400
+        elif pwCode == 4:
+            return {"Your password must have at least 1 uppercase letter."}, 400
+
+        try:
+            addUserToDB(data['user'], data['password'])
+        except:
+            return {"Error creating user."}, 500
+
+        return {'message': "Account created succesfully"}, 200
 
     def delete(self, id):
         return {'message': "Not developed yet"}, 404
--- data/UserQueries.py
+++ data/UserQueries.py
@@ -1,1 +1,40 @@
+import re
 from app.database import db
+
+def getAccountByEmail(email):
+    mycursor = db.cursor()
+
+    query = "SELECT * FROM Users WHERE email = %s"
+    values = (email,)
+    mycursor.execute(query, values)
+
+    myresult = mycursor.fetchall()
+
+    if len(myresult) == 0:
+        return 404
+
+    return _toJson(list(myresult[0]))
+
+def checkPasswords(password, repeat_password):
+
+    if password != repeat_password:
+        return 1
+    elif len(password) < 8:
+        return 2
+    elif re.search('[0-9]',password) is None:
+        return 3
+    elif re.search('[A-Z]',password) is None:
+        return 4
+
+    return 0
+
+def addUserToDB(email, password):
+    mycursor = db.cursor()
+    username = "User"
+    location = "BCN"
+    userphoto = 0
+    query = "INSERT INTO User (username, password, email, location, userphoto) " \
+            "VALUES (%s, %s, %s, %s, %d)"
+    values = (username, password, email, location, userphoto)
+    mycursor.execute(query, values)
+    db.commit()
