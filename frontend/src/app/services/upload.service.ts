2949a65109481330a510bab1f087de9423864ba1
DarkDhz <blazegammer24@gmail.com>
2021-11-22 16:25:02 +0100

    aadded s3 service

--- frontend/src/app/services/upload.service.ts
+++ frontend/src/app/services/upload.service.ts
@@ -8,4 +8,40 @@ import * as S3 from 'aws-sdk/clients/s3';
 export class UploadService {
 
   constructor() { }
+
+  uploadFile(file: File) {
+    const contentType = file.type;
+    const bucket = new S3(
+          {
+              accessKeyId: 'YOUR-ACCESS-KEY-ID',
+              secretAccessKey: 'YOUR-SECRET-ACCESS-KEY',
+              region: 'YOUR-REGION'
+          }
+      );
+      const params = {
+          Bucket: 'ubending',
+          Key: file.name,
+          Body: file,
+          ACL: 'public-read',
+          ContentType: contentType
+      };
+      bucket.upload(params, function (err: any, data: any) {
+          if (err) {
+              console.log('There was an error uploading your file: ', err);
+              return false;
+          }
+          console.log('Successfully uploaded file.', data);
+          return true;
+      });//for upload progress
+/*bucket.upload(params).on('httpUploadProgress', function (evt) {
+          console.log(evt.loaded + ' of ' + evt.total + ' Bytes');
+      }).send(function (err, data) {
+          if (err) {
+              console.log('There was an error uploading your file: ', err);
+              return false;
+          }
+          console.log('Successfully uploaded file.', data);
+          return true;
+      });*/
+}
 }
