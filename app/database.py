c4040829543eb9fddd97271cd6ac541d5b691604
DarkDhz <blazegammer24@gmail.com>
2021-11-23 10:31:40 +0100

    added new databse

--- app/database.py
+++ app/database.py
@@ -1,9 +1,24 @@
 import mysql.connector as connection
 
+
+host = "sql11.freesqldatabase.com"
+user = "sql11445855"
+password = "Xny8nFtYVz"
+database = "sql11445855"
+
+"""
+# db-aux
+host = "sql11.freesqldatabase.com"
+user = "sql11453270"
+password = "iedWsHbbSd"
+database = "sql11453270"
+
+"""
+
 db = connection.connect(
-    host="sql11.freesqldatabase.com",
-    user="sql11445855",
-    password="Xny8nFtYVz",
-    database="sql11445855"
+    host=host,
+    user=user,
+    password=password,
+    database=database
 )
 
