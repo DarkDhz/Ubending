78f88f65ddeb4953b57bc0331642ad58d70c2c22
Adria de Juan <adriadejuan22@gmail.com>
2021-11-24 13:31:26 +0100

    finished test_get_all_categories

--- tests/test_CategoryQueries.py
+++ tests/test_CategoryQueries.py
@@ -7,7 +7,20 @@ class TestCategoryQueries(TestCase):
         self.fail()
 
     def test_get_all_categories(self):
-        self.fail()
+        allCategories = [{'id': 1, 'name': 'Cars'},
+                         {'id': 2, 'name': 'Bikes'},
+                         {'id': 3, 'name': 'Toys'},
+                         {'id': 4, 'name': 'Home'},
+                         {'id': 5, 'name': 'Sports'},
+                         {'id': 6, 'name': 'Technology'},
+                         {'id': 7, 'name': 'Videogames'},
+                         {'id': 8, 'name': 'Clothes'},
+                         {'id': 9, 'name': 'Plants'},
+                         {'id': 10, 'name': 'Books & Music'},
+                         {'id': 11, 'name': 'Cinema'},
+                         {'id': 12, 'name': 'Pet adoption'}]
+
+        self.assertEqual(getAllCategories(), allCategories, "Error with getAllCategories().")
 
     def test_get_category_name_by_id(self):
         self.fail()
