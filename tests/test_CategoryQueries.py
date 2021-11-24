a807ec1186562858b1af972dcf5d78327c94f14f
Adria de Juan <adriadejuan22@gmail.com>
2021-11-24 14:07:37 +0100

    finished test__to_json

--- tests/test_CategoryQueries.py
+++ tests/test_CategoryQueries.py
@@ -1,10 +1,27 @@
 from unittest import TestCase
 from data.CategoryQueries import *
+from data.CategoryQueries import _toJson
 
 
 class TestCategoryQueries(TestCase):
     def test__to_json(self):
-        self.fail()
+        allCategories = [{'id': 1, 'name': 'Cars'},
+                         {'id': 2, 'name': 'Bikes'},
+                         {'id': 3, 'name': 'Toys'},
+                         {'id': 4, 'name': 'Home'},
+                         {'id': 5, 'name': 'Sports'},
+                         {'id': 6, 'name': 'Technology'},
+                         {'id': 7, 'name': 'Videogames'},
+                         {'id': 8, 'name': 'Clothes'},
+                         {'id': 9, 'name': 'Plants'},
+                         {'id': 10, 'name': 'Books & Music'},
+                         {'id': 11, 'name': 'Cinema'},
+                         {'id': 12, 'name': 'Pet adoption'}]
+        categoryNames = ["Cars", "Bikes", "Toys", "Home", "Sports",
+                      "Technology", "Videogames", "Clothes", "Plants",
+                      "Books & Music", "Cinema", "Pet adoption"]
+        for i in range(0,12):
+            self.assertEqual(_toJson([i+1, categoryNames[i]]), allCategories[i])
 
     def test_get_all_categories(self):
         allCategories = [{'id': 1, 'name': 'Cars'},
@@ -27,11 +44,11 @@ class TestCategoryQueries(TestCase):
         self.assertEqual(getCategoryNameByID(0), 404)
 
         # TRY EVERY OTHER CATEGORY
-        categories = ["Cars", "Bikes", "Toys", "Home", "Sports",
+        categoryNames = ["Cars", "Bikes", "Toys", "Home", "Sports",
                       "Technology", "Videogames", "Clothes", "Plants",
                       "Books & Music", "Cinema", "Pet adoption"]
         for i in range(0,12):
-            self.assertEqual(getCategoryNameByID(i+1), categories[i])
+            self.assertEqual(getCategoryNameByID(i+1), categoryNames[i])
 
     def test__category_to_json(self):
         self.fail()
