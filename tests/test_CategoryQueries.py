8e1c5b49d9a43877b76b6ccec1c2b1192f0b11ce
Adria de Juan <adriadejuan22@gmail.com>
2021-11-24 10:42:34 +0100

    generated test_CategoryQueries.py file

---
+++ tests/test_CategoryQueries.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from data.CategoryQueries import *
+
+
+class TestCategoryQueries(TestCase):
+    def test__to_json(self):
+        self.fail()
+
+    def test_get_all_categories(self):
+        self.fail()
+
+    def test_get_category_name_by_id(self):
+        self.fail()
+
+    def test__category_to_json(self):
+        self.fail()
+
+    def test_get_category_by_id(self):
+        self.fail()
