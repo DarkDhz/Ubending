5a3219bc12f1ed65e6e2bdf1bc97b1b4fa552617
Adria de Juan <adriadejuan22@gmail.com>
2021-11-24 14:28:56 +0100

    finished test_search_by_category

--- tests/test_SearchQueries.py
+++ tests/test_SearchQueries.py
@@ -3,7 +3,12 @@ from data.SearchQueries import *
 
 class TestSearchQueries(TestCase):
     def test_search_by_category(self):
-        self.fail()
+        technologyItems = [{'product_id': 17, 'owner_id': 1, 'name': 'Auriculares',
+                            'description': 'not des', 'price': 70, 'state': 'Destroyed',
+                            'image': 'jpeg', 'category_id': 'Technology'}, {'product_id': 18,
+                            'owner_id': 1, 'name': 'SmartWatch', 'description': 'new',
+                            'price': 200, 'state': 'Used', 'image': 'jpeg', 'category_id': 'Technology'}]
+        self.assertEqual(searchByCategory(6), technologyItems)
 
     def test_search_by_name(self):
         self.fail()
