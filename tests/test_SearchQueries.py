1ad8fa8b041e8fdcedb8f82d5c27f3cf88c56240
Adria de Juan <adriadejuan22@gmail.com>
2021-11-24 14:23:56 +0100

    generated test_SearchQueries.py file

---
+++ tests/test_SearchQueries.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from data.SearchQueries import *
+
+class TestSearchQueries(TestCase):
+    def test_search_by_category(self):
+        self.fail()
+
+    def test_search_by_name(self):
+        self.fail()
+
+    def test_search_by_category_and_name(self):
+        self.fail()
