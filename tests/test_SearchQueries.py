8b811b40eae9832a5f1afef59521c9393a864bdb
Adria de Juan <adriadejuan22@gmail.com>
2021-11-25 01:12:22 +0100

    finished test_search_by_name

--- tests/test_SearchQueries.py
+++ tests/test_SearchQueries.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from data.SearchQueries import *
+from data.UserQueries import *
+from data.ProductQueries import *
 
 class TestSearchQueries(TestCase):
     def test_search_by_category(self):
@@ -11,7 +13,15 @@ class TestSearchQueries(TestCase):
         self.assertEqual(searchByCategory(6), technologyItems)
 
     def test_search_by_name(self):
-        self.fail()
+        # SEARCH FOR UNEXISTING PRODUCT
+        self.assertEqual(searchByName("UnexistingProduct_FBJKSFHBJBFJKBJFBGKJBSFGBMSJKFHJKGSD"), 404)
+
+        # SEARCH FOR EXISTING PRODUCT
+        smartwatch = [{'product_id': 18, 'owner_id': 1, 'name': 'SmartWatch', 'description': 'new', 'price': 200, 'state': 'Used', 'image': 'jpeg', 'category_id': 'Technology'}]
+        self.assertEqual(searchByName("SmartWatch"), smartwatch)
+
+        # user and product should be created for testing purposes and then deleted when deleteProduct(productID, userID) and deleteUser(userID) work fine
 
     def test_search_by_category_and_name(self):
         self.fail()
+
