05c0acd2e3ccea16e82cb0e073f039421df2c518
Arnau Gris Garcia <31670015+DarkDhz@users.noreply.github.com>
2021-12-12 17:58:56 +0100

    frontend fixes

--- frontend/src/app/enviroment.ts
+++ frontend/src/app/enviroment.ts
@@ -3,6 +3,7 @@ export const environment = {
   //path: "https://ubending4.herokuapp.com"
   //dev
   path: 'https://ubendingdev.herokuapp.com'
+  //
   // local
   //path: "http://127.0.0.1:5000"
 }
