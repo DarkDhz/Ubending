8eb6f85724558484a8e4d3faab6eee5f3b19b01a
lrocarom <lrocarom@gmail.com>
2021-12-16 22:14:55 +0100

    fixDavid

--- data/ProductQueries.py
+++ data/ProductQueries.py
@@ -47,7 +47,7 @@ def getAllProductsOfUserByID(user_id):
     db = connection.connect(host=db_host, user=db_user, password=db_password, database=database)
     mycursor = db.cursor()
 
-    query = "SELECT * FROM Products WHERE owner_id = %s"
+    query = "SELECT * FROM Products WHERE owner_id = %s AND buyed = 0"
     values = (user_id,)
     mycursor.execute(query, values)
 
@@ -254,7 +254,7 @@ def getFollowingProduct(user_id, product_id):
     if len(myresult) == 0:
         return False
     return True
-  
+
 '''
 # add a rating
 
