3eda0fb3c42c0e7c5133b8e3a730db36d8a66f69
Arnau Gris Garcia <31670015+DarkDhz@users.noreply.github.com>
2021-12-17 13:03:34 +0100

    changed enviroment

--- frontend/src/app/enviroment.ts
+++ frontend/src/app/enviroment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   // deployment
-  //path: "https://ubending4.herokuapp.com"
+  path: "https://ubending4.herokuapp.com"
   //dev
-  path: 'https://ubendingdev.herokuapp.com'
+  //path: 'https://ubendingdev.herokuapp.com'
   //
   // local
   //path: "http://127.0.0.1:5000"
